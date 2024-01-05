bed9e1469e6aed763c2795c4559e58ebd162d5eb
LorenzoTamburi <S1096088@studenti.univpm.it>
2024-01-05 12:34:40 +0100

    non funziona filtraggio StoricoFragment

--- app/src/main/java/com/example/progettopm/fragments/mainFragments/StoricoFragment.kt
+++ app/src/main/java/com/example/progettopm/fragments/mainFragments/StoricoFragment.kt
@@ -5,66 +5,75 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.progettopm.R
+import com.example.progettopm.SessionManager
 import com.example.progettopm.model.Formazione
 import com.example.progettopm.view.StoricoAdapter
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 
-class StoricoFragment : Fragment(R.layout.fragment_storico) {
-    private lateinit var recyclerView: RecyclerView
+class StoricoFragment : Fragment() {
+
+    private lateinit var storicoRecycleView: RecyclerView
     private lateinit var storicoAdapter: StoricoAdapter
-    private lateinit var formazioniArrayList: ArrayList<Formazione>
-    private lateinit var db: FirebaseFirestore
+    private val formazioniList = mutableListOf<Formazione>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_storico, container, false)
-        recyclerView = view.findViewById(R.id.storicoRecycleView)
+        storicoRecycleView = view.findViewById(R.id.storicoRecycleView)
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupRecyclerView()
-        loadGiocatoriData()
+
+        // Recupero dei dati da Firestore
+        caricaFormazioni()
     }
 
-    private fun setupRecyclerView() {
-        storicoAdapter = StoricoAdapter()
-        recyclerView.apply {
-            layoutManager = LinearLayoutManager(activity)
-            //adapter = StoricoAdapter
+    private fun caricaFormazioni(){
+        //Controllo che venga scelto lo storico dell'utente loggato e della lega corrente
+        // Ottieni l'istanza dell'utente corrente
+        val user = FirebaseAuth.getInstance().currentUser
+        lateinit var userId: String
+        // Verifica se l'utente è attualmente autenticato
+        if (user != null) {
+            userId = user.uid.toString()
+            // Ora userId contiene l'ID dell'utente corrente
+        } else {
+            // L'utente non è attualmente autenticato
         }
-    }
 
-    private fun loadGiocatoriData() {
-        val formazioniCollection = db.collection("formazioni")
+        val legaId = SessionManager.legaCorrenteId
+        val formazioniCollection = FirebaseFirestore.getInstance().collection("formazioni")
 
-        formazioniCollection.get()
-            .addOnSuccessListener { documents ->
-                val datiFormazioneList: ArrayList<Formazione> = ArrayList()
-                for (document in documents) {
-                    val nomeSquadra = document.getString("nomeSquadra").toString()
-                    val giornataRef = document.get("giornata") as DocumentReference
-                    val giocatoriSchieratiRef = document.get("giocatoriSchierati") as DocumentReference
-                    val id = document.id
-                    val punteggio = document.getString("punteggio").toString()
-                    val userRef = document.get("user") as DocumentReference
-
-                    //val salvaDatiFormazione = Formazione(id, giornataRef, userRef, punteggio, nomeSquadra, giocatoriSchieratiRef)
-                    //datiFormazioneList.add(salvaDatiFormazione)
+        formazioniCollection
+            .whereEqualTo("id", userId)
+            .whereEqualTo("legaId", legaId)
+            .orderBy("giornata", Query.Direction.ASCENDING)
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val formazione = document.toObject(Formazione::class.java)
+                    formazioniList.add(formazione)
                 }
 
+                // Inizializzazione e associazione dell'adapter alla RecyclerView
+                storicoAdapter = StoricoAdapter(formazioniList)
+                storicoRecycleView.layoutManager = LinearLayoutManager(activity)
+                storicoRecycleView.adapter = storicoAdapter
             }
             .addOnFailureListener { exception ->
-                Log.w("StoricoFragment", "Errore nel recupero dei dati", exception)
+                Toast.makeText(requireContext(), "Errore nel recupero delle formazioni: $exception", Toast.LENGTH_SHORT).show()
             }
     }
-
-
 }
--- app/src/main/java/com/example/progettopm/view/StoricoAdapter.kt
+++ app/src/main/java/com/example/progettopm/view/StoricoAdapter.kt
@@ -1,4 +1,36 @@
 package com.example.progettopm.view
 
-class StoricoAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.progettopm.R
+import com.example.progettopm.model.Formazione
+import com.example.progettopm.model.Squadra
+
+class StoricoAdapter(private val formazioni: List<Formazione>) : RecyclerView.Adapter<StoricoAdapter.StoricoViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoricoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_storico, parent, false)
+        return StoricoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: StoricoViewHolder, position: Int) {
+        val formazione = formazioni[position]
+        holder.bind(formazione)
+    }
+
+    override fun getItemCount(): Int {
+        return formazioni.size
+    }
+
+     class StoricoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(formazione: Formazione) {
+            itemView.findViewById<TextView>(R.id.numGiornata_RV).text = formazione.giornata.toString()
+            // Assicurati che formazione.squadra contenga il nome della squadra
+            itemView.findViewById<TextView>(R.id.nomeSquadra_RV).text = formazione.squadra.toString() // Assuming formazione.squadra contains the team name
+            itemView.findViewById<TextView>(R.id.punteggioSquadra_RV).text = formazione.punteggio.toString()
+        }
+    }
 }
