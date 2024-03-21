adc834c422c95235868147a30bf92fd8155a8cdb
christianderrico <christian.derrico@studio.unibo.it>
2024-03-21 16:16:10 +0100

    added modification

--- app/src/main/java/com/example/progettopm/model/Bonus.kt
+++ app/src/main/java/com/example/progettopm/model/Bonus.kt
@@ -4,6 +4,7 @@ import com.google.firebase.firestore.DocumentReference
 
 data class Bonus(
     val valore: Int = 0,
+
     val nome: String = "",
     var id: String = "",
     val lega: DocumentReference? = null,
